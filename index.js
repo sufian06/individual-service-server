8a98a67523991b640bc67f30cc854f5b3859274a
sufian <sufian4se@gmail.com>
2022-11-10 20:15:20 +0600

    service detials by id api added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -37,6 +37,16 @@ async function run() {
 
       res.send(services);
     });
+
+    // service details api
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+
+
   } finally {
   }
 }
