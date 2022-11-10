e9bb419eca0b2642ad010b761f6f46545fdd0f91
sufian <sufian4se@gmail.com>
2022-11-10 21:41:44 +0600

    reviews api created from cient side

--- index.js
+++ index.js
@@ -46,6 +46,14 @@ async function run() {
       const service = await serviceCollection.findOne(query);
       res.send(service);
     });
+
+    // reviews api
+    app.post('/reviews', async(req, res) => {
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
+      res.send(result);
+    })
+
   } finally {
   }
 }
